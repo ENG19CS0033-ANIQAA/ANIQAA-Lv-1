b5701234c925ac056ca31fd56b94176ce6301f70
ENG19CS0033-ANIQAA <76059557+ENG19CS0033-ANIQAA@users.noreply.github.com>
2021-03-09 12:49:41 +0530

    Update add_with_4_functions.c

--- add_with_4_functions.c
+++ add_with_4_functions.c
@@ -1,17 +1,28 @@
 //Write a program to add two user input numbers using 4 functions.
 #include<stdio.h>
+int input()
+{
+int a;
+printf("Enter a Number\n");
+scanf("%d",&a);
+return a;
+}
+int find_sum(int p, int q)
+{
+int sum;
+sum =p+q;
+return sum;
+}
+void output(int p,int q,int r)
+{
+printf("Sum of %d + %d is %d\n",a,b,c);
+}
 int main()
 {
-int n,sum=0;
-printf("Enter the total number of numbers\n");
-scanf("%d",&n);
-int a[n];
-for(int i=0;i<n;i++)
-{
-printf("Enter %d number\n",(i+1));
-scanf("%d",&a[i]);
-sum=sum+a[i];
-}
-printf("The final sum is %d\n",sum);
+int a,b,c;
+a=input();
+b=input();
+c=find_sum(a,b);
+output(a,b,c)
 return 0;
 }
