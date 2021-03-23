9df62907206feb6219cca545b2e9420e20c79293
ENG19CS0033-ANIQAA <76059557+ENG19CS0033-ANIQAA@users.noreply.github.com>
2021-03-23 11:37:45 +0530

    first commit

--- distance.c
+++ distance.c
@@ -1,1 +1,30 @@
 //WAP to find the distance between two point using 4 functions.
+#include<stdio.h>
+float two_d(float y1,float y2,float z1,float z2)
+{
+ float dist;
+ dist=sqrt((y2-y1)(y2-y1)+(z2-z1)(z2-z1));
+ return dist;
+}
+float input()
+{
+ float n;
+ printf("enter the coordinates\n");
+ scanf("%f",&n);
+ return n;
+}
+void output(float p)
+{
+printf("the distance is %f",p);
+}
+int main()
+{
+ float p1,p2,q1,q2,1d;
+ p1=input();
+ p2=input();
+ q1=input();
+ q2=input();
+ 1d=two_d(a1,a2,b1,b2);
+ output(1d);
+ return 0;
+}
