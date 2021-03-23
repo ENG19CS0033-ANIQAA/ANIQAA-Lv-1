8e82557e83432161802f55bcff267f2319fb4e7b
ENG19CS0033-ANIQAA <76059557+ENG19CS0033-ANIQAA@users.noreply.github.com>
2021-03-23 10:43:27 +0530

    first commit

--- tromboloid_with_4_functions.c
+++ tromboloid_with_4_functions.c
@@ -1,1 +1,30 @@
 //WAP to find the volume of a tromboloid using 4 functions.
+#include<stdio.h>
+float input()
+{ 
+    float p;
+    printf("enter the values\n");
+    scanf("%f",&p);
+    return p;
+}
+float volume( float h,float b,float d)
+{
+   float volume;
+   volume=((h*b*d)+(b/d))/3;
+   return volume;
+}
+int output(float volume)
+{
+   printf("the volume of the tromboloid is %f",volume);
+   return 0;
+}
+int main()
+{
+   float a,b,c,vol
+   a=input();
+   b=input();
+   c=input();
+   vol=volume(a,b,c);
+   output(vol);
+   return 0
+}
